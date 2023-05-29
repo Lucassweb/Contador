d991896050a699787727ef3f73545d30da9a096c
Lucas <lucas.26.04@hotmail.com>
2023-05-29 17:22:47 -0300

    estilisando o conteudo

--- src/Container/index.tsx
+++ src/Container/index.tsx
@@ -1,11 +1,13 @@
-import {Content} from './style'
-
-
+import {Content, SecondContent, Botao} from './style'
 export const Container = () => {
-
     return (
         <Content>
             <h1>CONTADOR</h1>
+            <SecondContent>
+                <Botao>DIMINUI</Botao>
+                <Botao>RESET</Botao>
+                <Botao>ADICIONAR</Botao>
+            </SecondContent>
         </Content>
     )
 }
--- src/Container/style.tsx
+++ src/Container/style.tsx
@@ -1,7 +1,36 @@
 import styled from "styled-components"
 
 export const Content = styled.div`
-    font-family: 'Roboto', sans-serif;
-    border: 2px solid red;
+    max-width: 940px;
+    margin: auto;
+    margin-top: 10em;
+    width: 400px;
+    height: 30vh;
+    text-align: center;
+    border: 1px solid black;
+    border-radius: 10px;
 
+    h1 {
+        font-family: 'Roboto', sans-serif;
+        font-weight: bold;
+        color: purple;
+    }
+`
+
+export const SecondContent = styled.div`
+    display: flex;
+    margin-top: 10em;
+    justify-content: center;
+    justify-content: space-around;
+`
+
+export const Botao = styled.button`
+    width: 120px;
+    border-radius: 5px;
+    padding: 10px;
+    color: purple;
+    font-weight: bold;
+    background-color: #c2d1e6;
+    border: none;
+    cursor: pointer;
 `
